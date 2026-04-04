935796e40fccfd8747aff74a168ba63b19e48deb
Ammar-hadid <9017886@student.zadkine.nl>
2026-04-04 18:49:20 +0200

    op6

--- src/opdracht-06/ProfileCard.test.jsx
+++ src/opdracht-06/ProfileCard.test.jsx
@@ -12,21 +12,24 @@ describe('ProfileCard', () => {
 
   test('toont de leeftijd', () => {
     // TODO: Render ProfileCard met name="Piet" en age={30}
+    render(<ProfileCard name="Piet" age={30} />)
     // TODO: Check of tekst "Leeftijd: 30" op het scherm staat
     // Hint: gebruik screen.getByText()
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(screen.getByText("Leeftijd: 30")).toBeInTheDocument();
   });
 
   test('toont bio als die er is', () => {
     // TODO: Render ProfileCard met bio="Ik hou van programmeren"
+    render(<ProfileCard bio="Ik hou van programmeren" />)
     // TODO: Check of die tekst op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(screen.getByText("Ik hou van programmeren")).toBeInTheDocument();
   });
 
   test('toont fallback tekst als bio ontbreekt', () => {
     // TODO: Render ProfileCard ZONDER bio prop
+    render(<ProfileCard />)
     // TODO: Check of "Geen bio beschikbaar" op het scherm staat
-    expect(true).toBe(false); // Deze test faalt! Vervang met je eigen test
+    expect(screen.getByText("Geen bio beschikbaar")).toBeInTheDocument();
   });
 
 });
